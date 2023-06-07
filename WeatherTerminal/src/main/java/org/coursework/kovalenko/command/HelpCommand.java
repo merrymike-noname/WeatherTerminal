123a2984c6e8454b2f38706204d3e8bcb9ca3436
Mykhailo Kovalenko <kovalenko.mykhailo1@lll.kpi.ua>
2023-06-08 00:50:04 +0300

    Update "-help" command, update README.md

--- WeatherTerminal/src/main/java/org/coursework/kovalenko/command/HelpCommand.java
+++ WeatherTerminal/src/main/java/org/coursework/kovalenko/command/HelpCommand.java
@@ -5,6 +5,6 @@ public class HelpCommand implements Command{
     public String buildMessage() {
         return "Here is the list of commands:\n|\t-cw -- get current weather\n|\t-tw -- weather till the end of this day" +
                 "\n|\t-cdw -- choose a day to get its weather at 12 p.m.\n|\t-fdf -- get five day forecast (for 12 p.m. each day)" +
-                "\n|\t-help -- get list of commands\n|\t-ex -- exit";
+                "\n|\t-help -- get list of commands\n|\t-city -- change city\n|\t-ex -- exit";
     }
 }
